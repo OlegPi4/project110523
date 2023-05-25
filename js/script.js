e21302dd20688e3bb641e3194eea00039eb15ebb
OlegPichkur <oleg1pichkur@gmail.com>
2023-05-25 15:18:14 +0300

    fatch на отправку и получение, карточки из серверв

--- js/script.js
+++ js/script.js
@@ -197,35 +197,47 @@ window.addEventListener('DOMContentLoaded', () => {
          this.parent.append(element);
       }
    }
-   // вызов карточки без создания переменной
-   new MenuCard(
-      "img/tabs/vegy.jpg",
-      "vegy",
-      'Меню "Фитнес"',
-      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-      9,
-      '.menu .container',
-      'menu__item',
-      'big'
-   ).render();
-   new MenuCard(
-      "img/tabs/elite.jpg",
-      "elite",
-      'Меню “Премиум”',
-      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная  рыба,  морепродукты, фрукты -   ресторанное меню без похода в ресторан!',
-      14,
-      '.menu .container',
-      'menu__item'
-   ).render();
-   new MenuCard(
-      "img/tabs/post.jpg",
-      "post",
-      'Меню "Постное"',
-      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-      12,
-      '.menu .container',
-      'menu__item'
-   ).render();
+
+   // выводим fetch в отдельную функцию для получения данных с json сервера   
+   const getResourse = async (url) => {
+      const res = await fetch(url);
+
+      if (!res.ok) { 
+         throw new Error(`Coudn't fetch ${url}, status ${res.status}`);
+      }
+      return await res.json();
+   };
+   // формирование карточек меню из данных сервера json
+   // вариант 1 - предпочтительный с исп. классов
+   getResourse('http://localhost:3000/menu')
+      .then(data => {
+         data.forEach(({ img, altimg, title, descr, price }) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+         });
+      });
+
+   // вариант 2 - для формирования верстки с ходу (без использования классов)
+
+   // const courseOfCurrency = 36.93; // привязка к курсу для цены в карточках
+   // getResourse('http://localhost:3000/menu')
+   //    .then(data => creatCard(data));
+   // function creatCard(data) {
+   //    data.forEach(({ img, altimg, title, descr, price }) => {
+   //       const element = document.createElement('div');
+   //       element.classList.add('menu__item');
+   //       element.innerHTML = `
+   //          <img src=${img} alt=${altimg}>
+   //             <h3 class="menu__item-subtitle">${title}</h3>
+   //             <div class="menu__item-descr">${descr}</div>
+   //             <div class="menu__item-divider"></div>
+   //             <div class="menu__item-price">
+   //                <div class="menu__item-cost">Цена:</div>
+   //                <div class="menu__item-total"><span>${price*courseOfCurrency}</span> грн/день</div>
+   //             </div>
+   //       `;
+   //       document.querySelector('.menu .container').append(element);
+   //    });
+   // }
 
    // Берем данные из форм на сайте и отправляем на сервер 
    const forms = document.querySelectorAll('form');
@@ -237,10 +249,22 @@ window.addEventListener('DOMContentLoaded', () => {
    };
    
    forms.forEach(item => {
-      postData(item);
+      bindPostData(item);
    });
+// выводим fetch в отдельную функцию для отправки данных   
+   const postData = async (url, data) => {
+      const res = await fetch(url, {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json'
+         },
+         body: data
+      });
+      // возвращаем промис res в json формате 
+      return await res.json();
+   };
 
-   function postData(form) {
+   function bindPostData(form) {
       form.addEventListener('submit', (e) => {
          e.preventDefault(); // отменяем стандартное поведение браузера (перезагрузка при submit)
          // добавляем в form отражение статуса запроса spinner
@@ -252,22 +276,15 @@ window.addEventListener('DOMContentLoaded', () => {
          `;
          
          form.insertAdjacentElement('afterend', statusMessage);
-         // Реализация с Fetch
-         
+               
          const formData = new FormData(form); 
-         const object = {};
-         formData.forEach(function (value, key) {
-            object[key] = value;
-         });
          
-         fetch('server.php', {
-            method: 'POST',
-            headers: {
-               'Content-type': 'application/json'
-            },
-            body: JSON.stringify(object)
-         })
-         .then(data => data.text())
+         const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      
+         // вызов fetch ч-з функцию postData. отправка данных на сервер
+         // указывается на json-server в db.json в массив request 
+         // это постинг данных в формате json на json-сервер
+         postData('http://localhost:3000/requests', json)
          .then(data => {
             console.log(data);
             showThanksModal(messag.success); // вызов функции показа сообщения об отправке
@@ -309,4 +326,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }, 4000);
    }
    
+   fetch('http://localhost:3000/menu')
+      .then(data => data.json())
+      .then(res => console.log(res));
 });
