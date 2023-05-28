d533a89b008a38fe8246951ee196c8c0eea59d89
OlegPichkur <oleg1pichkur@gmail.com>
2023-05-28 22:49:48 +0300

    js скрипт калькулятора  ккалорий

--- js/script.js
+++ js/script.js
@@ -306,7 +306,7 @@ window.addEventListener('DOMContentLoaded', () => {
    let slideIndex = 1;
    let offset = 0; 
 
-   function showCounterSlide(curr = 1) {
+   function showCounterSlide(curr = 1) { // выводим текущий слайд в счетчик
       if (slides.length < 10) {
          current.textContent = `0${curr}`;
       } else {
@@ -314,10 +314,13 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    }
 
-   function showDots(dots) {
+   function showDots(dots) {  // формируем точки навигации слайдера
       dots.forEach(dot => dot.style.opacity = '0.5');
       dots[slideIndex - 1].style.opacity = 1;
    }
+   function clearValue(val) { // преобразовываем символьное значение в рх в число
+      return +val.replace(/\D/g, '');
+   } 
    
    slidesField.style.width = 100 * slides.length + '%';
    slidesField.style.display = 'flex';
@@ -357,10 +360,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
    next.addEventListener('click', () => {
       // offset - задает смещение ленты слайдов для показа текущего
-      if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      //if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
+         if (offset == clearValue(width) * (slides.length - 1)) {
          offset = 0;
       } else {
-         offset += +width.slice(0, width.length - 2);
+         offset += clearValue(width);
       }
       // выполняем смещение
       slidesField.style.transform = `translateX(-${offset}px)`;
@@ -374,9 +378,9 @@ window.addEventListener('DOMContentLoaded', () => {
    });
    prev.addEventListener('click', () => {
       if (offset == 0) {
-         offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+         offset = clearValue(width) * (slides.length - 1);
       } else {
-         offset -= +width.slice(0, width.length - 2);
+         offset -= clearValue(width);
       }
       slidesField.style.transform = `translateX(-${offset}px)`;
       if (slideIndex == 1) {
@@ -388,19 +392,118 @@ window.addEventListener('DOMContentLoaded', () => {
       showDots(dots);
    });
    // функционал клика на точку 
-   dots.forEach(dot => {    
+   dots.forEach(dot => {
       dot.addEventListener('click', (e) => {
-         const slideTo = e.target.getAttribute('data-slide-to'); 
+         const slideTo = e.target.getAttribute('data-slide-to');
          slideIndex = slideTo; // изменяем счетчик на текущий слайд
          // расчитываем смещение
-         offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+         offset = +width.replace(/\D/g, '') * (slideTo - 1);
          // делаем сдвиг карусели слайдов
          slidesField.style.transform = `translateX(-${offset}px)`;
-         // ставим ведущий 0
+         // ставим ведущий 0 и выводим счетчик
          showCounterSlide(slideIndex);
          showDots(dots);
+      });
+   });
+   // Калькулятор калорий
+   const result = document.querySelector('.calculating__result span');
+   let sex, height, weight, age, ratio;
+   // устанавливаем начальное значение пол и активность
+   if (localStorage.getItem('sex')) {
+      sex = localStorage.getItem('sex');
+   } else {
+      sex = 'female';
+      localStorage.setItem('sex', 'female');
+   }
+   if (localStorage.getItem('ratio')) {
+      ratio = localStorage.getItem('ratio');
+   } else {
+      ratio = 1.375;
+      localStorage.setItem('ratio', 1.375);
+   }
+   // установка класа активности в соотвецтвии с localStorage
+   function initLocalSettings(selector, activeClass) {
+      const elements = document.querySelectorAll(selector);
+
+      elements.forEach(elem => {
+         elem.classList.remove(activeClass);
+         if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+            elem.classList.add(activeClass);
+         } 
+         if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+            elem.classList.add(activeClass);
+         }
       })
-   })
+   }
+   initLocalSettings('#gender div', 'calculating__choose-item_active');
+   initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+   function calcTotal() {
+      if (!sex || !height || !weight || !age || !ratio) {
+         result.textContent = '____';
+         return;
+      }
+      if (sex === 'female') {
+         result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+      } else {
+         result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+      }
+   }
+   calcTotal(); 
+   // получение статической информации пол и физ, активность
+   function getStaticInformation(selector, activeClass) {
+      const elements = document.querySelectorAll(selector);
+
+      elements.forEach(elem => {
+         elem.addEventListener('click', (e) => {
+            if (e.target.getAttribute('data-ratio')) {
+               ratio = e.target.getAttribute('data-ratio');
+               // добавим значения в localStorage
+               localStorage.setItem('ratio', e.target.getAttribute('data-ratio'));
+            } else {
+               sex = e.target.getAttribute('id');
+               localStorage.setItem('sex', e.target.getAttribute('id'));
+            }
+            // отмечаем по клику клас активности и считаем
+            elements.forEach(elem => {
+               elem.classList.remove(activeClass);
+            });
+            e.target.classList.add(activeClass);
+            calcTotal();
+         });
+      });
+   }
+   getStaticInformation('#gender div', 'calculating__choose-item_active');
+   getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+
+   // принимаем значеия input
+   function getDynamicInformation(selector) {
+      const input = document.querySelector(selector);
+      input.addEventListener('input', () => {
+         // подсвечиваем красным, если вводятся не цифры
+         if (input.value.match(/\D/g)) {
+            input.style.border = '1px solid red';
+         } else {
+            input.style.border = 'none';
+         }
+
+         switch (input.getAttribute('id')) {
+            case 'height':
+               height = +input.value;
+               break;
+            case 'weight':
+               weight = +input.value;
+               break;
+            case 'age':
+               age = +input.value;
+               break;
+         }
+         calcTotal();
+      });
+   }
+   getDynamicInformation('#height');
+   getDynamicInformation('#weight');
+   getDynamicInformation('#age');
 
 });
 
